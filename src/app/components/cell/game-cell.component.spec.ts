c6a92d6ed5e7bb6b17272804317500c713ab20ed
Christian Heredia <cheredianaranjo.it@hbplat.com>
2025-05-19 01:13:14 +0200

    fix(test): fix unit test

--- src/app/components/cell/game-cell.component.spec.ts
+++ src/app/components/cell/game-cell.component.spec.ts
@@ -46,25 +46,25 @@ describe('GameCellComponent', () => {
   });
 
   it('should return correct bow image (bow.svg) for hunter with arrows and no gold', () => {
-    expect(component.bowImage()).toBe('bow.svg');
+    expect(component.bowImage()).toContain('bow.svg');
   });
 
   it('should return bowgold.svg if hunter has gold and arrows', () => {
     fixture.componentRef.setInput('hunter', { ...mockHunter, hasGold: true });
     fixture.detectChanges();
-    expect(component.bowImage()).toBe('bowgold.svg');
+    expect(component.bowImage()).toContain('bowgold.svg');
   });
 
   it('should return bowgoldempty.svg if hunter has gold but no arrows', () => {
     fixture.componentRef.setInput('hunter', { ...mockHunter, hasGold: true, arrows: 0 });
     fixture.detectChanges();
-    expect(component.bowImage()).toBe('bowgoldempty.svg');
+    expect(component.bowImage()).toContain('bowgoldempty.svg');
   });
 
   it('should return bowempty.svg if hunter has no gold and no arrows', () => {
     fixture.componentRef.setInput('hunter', { ...mockHunter, hasGold: false, arrows: 0 });
     fixture.detectChanges();
-    expect(component.bowImage()).toBe('bowempty.svg');
+    expect(component.bowImage()).toContain('bowempty.svg');
   });
 
   function setHunterDirection(direction: number) {
