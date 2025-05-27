257edd8714f91a6155dd7e6cbb8ec48fe79dadfa
Christian Heredia <cheredianaranjo.it@hbplat.com>
2025-05-27 23:45:49 +0200

    test(unit): add unit test

--- src/app/store/game-store.spec.ts
+++ src/app/store/game-store.spec.ts
@@ -132,40 +132,42 @@ describe('GameStore (SignalStore)', () => {
   });
 
   it('should initialize the board and hunter correctly on initBoard', () => {
-  store.setSettings(mockSettings);
-  store.updateHunter({ ...mockHunter });
-  store.initBoard();
+    store.setSettings(mockSettings);
+    store.updateHunter({ ...mockHunter });
+    store.initBoard();
 
-  const board = store.board();
-  const hunter = store.hunter();
+    const board = store.board();
+    const hunter = store.hunter();
 
-  expect(board.length).toBe(mockSettings.size);
-  expect(board[0].length).toBe(mockSettings.size);
+    expect(board.length).toBe(mockSettings.size);
+    expect(board[0].length).toBe(mockSettings.size);
 
-  const contents = board.flat().map((cell: Cell) => cell.content).filter(Boolean);
-  const hasGold = contents.includes('gold');
-  expect(hasGold).toBe(false);
+    const contents = board
+      .flat()
+      .map((cell: Cell) => cell.content)
+      .filter(Boolean);
+    const hasGold = contents.includes('gold');
+    expect(hasGold).toBe(false);
 
-  expect(hunter.x).toBe(0);
-  expect(hunter.y).toBe(0);
-  expect(hunter.direction).toBe(Direction.RIGHT);
-});
+    expect(hunter.x).toBe(0);
+    expect(hunter.y).toBe(0);
+    expect(hunter.direction).toBe(Direction.RIGHT);
+  });
 
-it('should correctly set hunter for next level', () => {
-  store.setSettings(mockSettings);
-  store.updateHunter({ ...mockHunter, lives: 6 });
-  store.setHunterForNextLevel();
+  it('should correctly set hunter for next level', () => {
+    store.setSettings(mockSettings);
+    store.updateHunter({ ...mockHunter, lives: 6 });
+    store.setHunterForNextLevel();
 
-  const hunter = store.hunter();
+    const hunter = store.hunter();
 
-  expect(hunter.x).toBe(0);
-  expect(hunter.y).toBe(0);
-  expect(hunter.direction).toBe(Direction.RIGHT);
-  expect(hunter.arrows).toBe(mockSettings.arrows);
-  expect(hunter.hasGold).toBe(false);
-  expect(hunter.hasWon).toBe(false);
-  expect(hunter.alive).toBe(true);
-  expect(hunter.lives).toBeLessThanOrEqual(mockSettings.difficulty.maxLives);
-});
-
+    expect(hunter.x).toBe(0);
+    expect(hunter.y).toBe(0);
+    expect(hunter.direction).toBe(Direction.RIGHT);
+    expect(hunter.arrows).toBe(mockSettings.arrows);
+    expect(hunter.hasGold).toBe(false);
+    expect(hunter.hasWon).toBe(false);
+    expect(hunter.alive).toBe(true);
+    expect(hunter.lives).toBeLessThanOrEqual(mockSettings.difficulty.maxLives);
+  });
 });
