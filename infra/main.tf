7cf21088c2b19ff3d0e278139de57bb5c066f9d8
Christian Heredia <cheredianaranjo.it@hbplat.com>
2025-05-28 15:57:41 +0200

    feat(): testing terraform

---
+++ infra/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "cursosfrontend"
+
+    workspaces {
+      name = "hunt-the-bishomalo"
+    }
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
+resource "github_repository_pages" "pages" {
+  repository = var.repo_name
+  source {
+    branch = "gh-pages"
+    path   = "/"
+  }
+
+  build_type = "workflow"
+}
