0d3e3bb5f0faa8faae32b8573558c52930cc812c
Christian Heredia <cheredianaranjo.it@hbplat.com>
2026-03-08 10:12:09 +0100

    fix: lint

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,9 +2,7 @@ export * from './404/not-found.component';
 export { CharactersComponent } from '@hunt-the-bishomalo/chars';
 export * from './hunt-bisho.component';
 export { ShopComponent } from '@hunt-the-bishomalo/shop';
-export {
-  EndCreditsComponent,
-} from '@hunt-the-bishomalo/credits';
+export { EndCreditsComponent } from '@hunt-the-bishomalo/credits';
 export { InstructionsComponent } from '@hunt-the-bishomalo/instructions';
 export { ResultsComponent } from '@hunt-the-bishomalo/gamestats';
 export { BossFightComponent } from '@hunt-the-bishomalo/boss';
