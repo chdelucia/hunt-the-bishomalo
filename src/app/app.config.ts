648c608f6a6d6e4b6ed3f019b4fe75de7ec97f47
Christian Heredia <cheredianaranjo.it@hbplat.com>
2026-03-16 22:09:08 +0100

    fix: lint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,11 @@ import { TranslocoHttpLoader } from './utils/transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
 import { GAME_STORE } from '@hunt-the-bishomalo/story/api';
 import { GameStore } from '@hunt-the-bishomalo/core/store';
-import { LEADERBOARD_SERVICE, ACHIEVEMENT_SERVICE, GAME_ENGINE_TOKEN } from '@hunt-the-bishomalo/core/services';
+import {
+  LEADERBOARD_SERVICE,
+  ACHIEVEMENT_SERVICE,
+  GAME_ENGINE_TOKEN,
+} from '@hunt-the-bishomalo/core/services';
 import { AchievementService } from '@hunt-the-bishomalo/achievements/data-access';
 import { LeaderboardService } from '@hunt-the-bishomalo/gamestats/data-access';
 import { GameEngineService } from '@hunt-the-bishomalo/game/data-access';
